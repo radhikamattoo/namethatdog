2446a8fc2baf723795318b28fc77fbd420617bff
Radhika Mattoo <rm3485@nyu.edu>
2018-05-31 23:10:58 -0400

    adding annotations to get_data

--- get_data.py
+++ get_data.py
@@ -1,22 +1,27 @@
-# Radhika Mattoo, rm3485@nyu.edu
-# This file downloads and prepares the Stanford Dogs Dataset
-import requests
+# -*- coding: utf-8 -*-
+# Radhika Mattoo
+# Downloads and prepares the Stanford Dogs Dataset
+from bs4 import BeautifulSoup
 import scipy.io as sio
+import requests
 import tarfile
 import os
-
-# URLs for downloading data
-urls = [
-    'http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar',
-    'http://vision.stanford.edu/aditya86/ImageNetDogs/lists.tar'
-]
-# Relative path of file destinations
-download_destinations = ['data/tars/images.tar', 'data/tars/lists.tar']
-final_destinations = ['data/', 'data/lists']
+import sys
 
 # Downloads and untars necessary data
 def get_data():
-    # Download and untar files
+    # URLs for downloading data
+    urls = [
+        'http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar',
+        'http://vision.stanford.edu/aditya86/ImageNetDogs/lists.tar',
+        'http://vision.stanford.edu/aditya86/ImageNetDogs/annotation.tar'
+    ]
+
+    # Relative path of file destinations
+    download_destinations = ['data/tars/images.tar', 'data/tars/lists.tar', 'data/annotations/annotation.tar']
+    final_destinations = ['data/', 'data/lists', 'data/']
+
+    # Download and untar files only if dirs don't exist
     for idx, url in enumerate(urls):
         # Create directories if they don't exist
         download_destination = download_destinations[idx]
@@ -34,19 +39,65 @@ def get_data():
         tar = tarfile.open(download_destination)
         tar.extractall(final_destination)
         tar.close()
+
     # Rename Images to images (from images.tar extraction)
     if os.path.exists('data/Images'):
         os.rename('data/Images', 'data/images')
+    if os.path.exists('data/Annotation'):
+        os.rename('data/Annotation', 'data/annotations')
 
-# Helper function for creating directories
-def mkdirs(dirnames):
-    for dirname in dirnames:
-        if not os.path.exists(os.path.dirname(dirname)):
-            try:
-                os.makedirs(os.path.dirname(dirname))
-            except OSError as exc:
-                if exc.errno != errno.EEXIST:
-                    raise
+# Splits images into train/test directories
+def split_data():
+    print '\nSplitting data into train/test directories'
+
+    # Read in train/test lists
+    train_dict = sio.loadmat('data/lists/train_list.mat')['annotation_list']
+    test_dict = sio.loadmat('data/lists/test_list.mat')['annotation_list']
+    train_list, test_list = convert_to_list(train_dict), convert_to_list(test_dict)
+    mkdirs( ['data/train', 'data/val'] )
+
+    # TODO: Read in annotations
+    # Walk through annotations/ directory
+    # Get dog breed from directory name
+    # Make train/test dir with breed name
+    # Walk through xml files in each directory
+    # Check if file is in train or test and rename
+
+    # for download_path in train_list:
+    #     if os.path.exists(download_path):
+    #         filename = os.path.basename(download_path)
+    #         train_path = os.path.abspath(os.path.join('data/train/', filename))
+    #         print train_path
+    #         print download_path
+    #         os.rename(download_path, train_path)
+    #     else:
+    #         print 'File not found: ', download_path
+    # for download_path in test_list:
+    #     if os.path.exists(download_path):
+    #         filename = os.path.basename(download_path)
+    #         test_path = os.path.abspath(os.path.join('data/test/', filename))
+    #         os.rename(download_path, test_path)
+    #     else:
+    #         print 'File not found: ', download_path
+
+# Create the given directories
+def mkdirs(names):
+    for name in names:
+        dirname = os.path.dirname(name) if os.path.isfile(name) else name
+        if not os.path.exists(dirname):
+            os.makedirs(dirname)
+
+# Collates items from the Matlab dict into a list
+def convert_to_list(matlab_dict):
+    file_list = []
+    for item in matlab_dict:
+        filepath = item[0][0]
+        file_list.append( os.path.abspath('data/images/' + filepath + '.jpg') )
+    return file_list
+
 
 if __name__ == '__main__':
-    get_data()
+    if not os.path.exists('data/tars'):
+        get_data()
+    # if not os.path.exists('data/train') and not os.path.exists('data/test'):
+    split_data()
--- model.py
+++ model.py
@@ -1,4 +1,51 @@
 # -*- coding: utf-8 -*-
-
+# Radhika Mattoo
+#
+import time
+import os
+import copy
 import torch
 import torchvision
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import torchvision.models as models
+from torch.optim import lr_scheduler
+from torchvision import datasets, models, transforms
+
+# inception = models.inception_v3(pretrained=True)
+# print inception
+# Prepare the data for Inception
+
+
+# print 'train dict:'
+# print train_dict
+# print '\ntest dict:'
+# print test_dict
+# data_transforms = {
+#     'train': transforms.Compose([
+#         transforms.RandomResizedCrop(224),
+#         transforms.RandomHorizontalFlip(),
+#         transforms.ToTensor(),
+#         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#     ]),
+#     'val': transforms.Compose([
+#         transforms.Resize(256),
+#         transforms.CenterCrop(224),
+#         transforms.ToTensor(),
+#         transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+#     ]),
+# }
+#
+# data_dir = 'hymenoptera_data'
+# image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+#                                           data_transforms[x])
+#                   for x in ['train', 'val']}
+# dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+#                                              shuffle=True, num_workers=4)
+#               for x in ['train', 'val']}
+# dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+# class_names = image_datasets['train'].classes
+#
+# device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
